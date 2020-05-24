85676b1145c15750b6afbcaf6b8d01ff1f97b80f
niming175 <niming175@qq.com>
2020-05-24 16:33:39 +0800

    修改update文件

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ fi
 
 echo "version: $hash"
 
-sed -i.bak "/app-backend:/s/\(app-backend:\)\([^\"]*\)/\1${$hash}/" docker-compose.yml
+sed -i.bak "/app-backend:/s/\(app-backend:\)\([^\"]*\)/\1${hash}/" docker-compose.yml
 sed -i.bak "/app-nginx:/s/\(app-nginx:\)\([^\"]*\)/\1${hash}/" docker-compose.yml
 
 docker-compose pull
